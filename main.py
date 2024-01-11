e44d1efe0339681b0d353b5fcc17d0a65164ac18
Halil-Tunc <149342519+Halil-Tunc@users.noreply.github.com>
2024-01-11 01:54:30 +0000

    I coded the placeholder options

--- main.py
+++ main.py
@@ -5,28 +5,66 @@ age = input ("How old are you? ")
 
 if int(age) == 13 or int(age) < 13:
   print (f"What I wouldn't give to be {age} again!")
+  
+  print (f"How can I help you today?")
+  print (f"1. Where can I find the cheapest toys?")
+  print (f"2. Where can I find the cheapest video games?")
+  print (f"3. Where can I find the cheapest game consoles?")
+  print (f"4. Exit conversation")
+  choice = input ("Enter the given number of your choice: ")
+
+  if choice == "1":
+    print (f"This is meant to be a place holder for a continuation in the future.")
+  if choice == "2":
+    print (f"This is meant to ba a place holder for a continuation in the future.")
+  if choice =="3":
+    print (f"This is meant to be a place holder for a continuation in the future.")
+  if choice == "4":
+    print (f"Goodbye, {name}.")
+    exit()
+
 if int(age) == 14 or int(age) < 18 and int(age) > 14:
   print (f"The good old days!")
+  
+  print (f"How can I help you today?")
+  print (f"1. Where can I get my learners permit?")
+  print (f"2. Where can I get my drivers license?")
+  print (f"3. Where can I get employed?")
+  print (f"4. Exit conversation")
+  choice = input ("Enter the given number of your choice: ")
+
+  if choice == "1":
+    print (f"This is meant to be a place holder for a continuation in the future.")
+  if choice == "2":
+    print (f"This is meant to ba a place holder for a continuation in the future.")
+  if choice =="3":
+    print (f"This is meant to be a place holder for a continuation in the future.")
+  if choice == "4":
+    print (f"Goodbye, {name}.")
+    exit()
+    
 if int(age) == 18 or int(age) > 18 and int(age) < 125:
   print (f"time flies by, doesn't it?")
+  
+  print (f"How can I help you today?")
+  print (f"1. When can I retire?")
+  print (f"2. Where can I apply for the military?")
+  print (f"3. Where can I vote?")
+  print (f"4. Exit conversation")
+  choice = input ("Enter the given number of your choice: ")
+
+  if choice == "1":
+    print (f"This is meant to be a place holder for a continuation in the future.")
+  if choice == "2":
+    print (f"This is meant to ba a place holder for a continuation in the future.")
+  if choice =="3":
+    print (f"This is meant to be a place holder for a continuation in the future.")
+  if choice == "4":
+    print (f"Goodbye, {name}.")
+    exit()
+    
 if int(age) == 125 or int(age) > 125:
   print (f"You've gotta be kidding me, the oldest a human can be is 125.")
 if not(int(age)<=0) or not(int(age)>=0):
   print (f"Please enter a real number.")
 
-print (f"How can I help you today?")
-print (f"1. Place holder 1")
-print (f"2. Place holder 2")
-print (f"3. Place holder 3")
-print (f"4. Exit conversation")
-choice = input ("Enter your choice: ")
-
-if choice == "1":
-  print (f"This is meant to be a place holder for a continuation in the future.")
-if choice == "2":
-  print (f"This is meant to ba a place holder for a continuation in the future.")
-if choice =="3":
-  print (f"This is meant to be a place holder for a continuation in the future.")
-if choice == "4":
-  print (f"Goodbye, {name}.")
-  exit()
